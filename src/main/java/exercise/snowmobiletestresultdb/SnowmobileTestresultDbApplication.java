da62817d8678a591e07542dab317870f352bdcab
tqre <tkuure@gmail.com>
2020-03-26 19:58:28 +0200

    Repo init

---
+++ src/main/java/exercise/snowmobiletestresultdb/SnowmobileTestresultDbApplication.java
@@ -0,0 +1,13 @@
+package exercise.snowmobiletestresultdb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SnowmobileTestresultDbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SnowmobileTestresultDbApplication.class, args);
+	}
+
+}
---
+++ src/test/java/exercise/snowmobiletestresultdb/SnowmobileTestresultDbApplicationTests.java
@@ -0,0 +1,13 @@
+package exercise.snowmobiletestresultdb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SnowmobileTestresultDbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
