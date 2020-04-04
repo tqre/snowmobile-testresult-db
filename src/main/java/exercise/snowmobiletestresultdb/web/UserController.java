1731fc116500761fff09ca657735bda9647f6e70
tqre <tkuure@gmail.com>
2020-04-04 19:48:41 +0300

    User view for admins

--- src/main/java/exercise/snowmobiletestresultdb/web/UserController.java
+++ src/main/java/exercise/snowmobiletestresultdb/web/UserController.java
@@ -2,6 +2,7 @@ package exercise.snowmobiletestresultdb.web;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import exercise.snowmobiletestresultdb.domain.UserRepository;
@@ -11,6 +12,12 @@ public class UserController {
 
 	@Autowired
 	private UserRepository uRepo;
+	
+	@RequestMapping("/all_users")
+	public String showAllUsers(Model model) {
+		model.addAttribute("get_all", uRepo.findAll());
+		return "all_users";
+	}
 	
 	@RequestMapping(value="/login")
 	public String login() {
