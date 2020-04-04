2cbce696ad12fee471cf7c3fdc316cfe819feb12
tqre <tkuure@gmail.com>
2020-04-04 19:31:47 +0300

    Navigation bar works now

--- src/main/java/exercise/snowmobiletestresultdb/web/UserController.java
+++ src/main/java/exercise/snowmobiletestresultdb/web/UserController.java
@@ -1,11 +1,17 @@
 package exercise.snowmobiletestresultdb.web;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import exercise.snowmobiletestresultdb.domain.UserRepository;
 
 @Controller
 public class UserController {
 
+	@Autowired
+	private UserRepository uRepo;
+	
 	@RequestMapping(value="/login")
 	public String login() {
 		return "login";
