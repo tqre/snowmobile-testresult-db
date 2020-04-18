68892e66a655256d674c974e760ba656eff987d1
tqre <tkuure@gmail.com>
2020-04-18 19:04:53 +0300

    Working error reports finally!

--- src/main/java/exercise/snowmobiletestresultdb/web/SnowMobileController.java
+++ src/main/java/exercise/snowmobiletestresultdb/web/SnowMobileController.java
@@ -8,8 +8,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
@@ -28,20 +29,20 @@ public class SnowMobileController {
 		return "all_snowmobiles";
 	}
 	
-	@RequestMapping(value="/add_snowmobile", method=RequestMethod.GET)
+	@GetMapping("/add_snowmobile")
 	public String addSnowMobile(Model model) {
-		model.addAttribute("snowmobile", new SnowMobile());
+		model.addAttribute("snowMobile", new SnowMobile());
 		return "add_snowmobile";
 	}
-	
-	@RequestMapping(value="/save_snowmobile", method=RequestMethod.POST)
-	public String saveSnowMobile(@Valid SnowMobile snowmobile,
+	// REALLY: it has to read snowMobile!
+	// Consistency in naming, but not expecting this...
+	@PostMapping("/save_snowmobile")
+	public String checkSnowMobile(@Valid SnowMobile snowMobile,
 			BindingResult bindingResult, Model model) {
-		model.addAttribute("snowmobile", snowmobile);
 		if (bindingResult.hasErrors()) {
 			return "add_snowmobile";
 		}
-		smRepo.save(snowmobile);
+		smRepo.save(snowMobile);
 		// TODO: notify user for success would be nice
 		return "redirect:all_snowmobiles";
 	}
